f8bfd8669c065e63bf2c8073ac1a7eef23b78873
frankcoded <francissteven578@yahoo.com>
2022-10-28 23:33:23 -0800

    first commit

--- Server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-http.createServer(function(req,res){
-    res.writeHead(200,{ 'content-type':'text/html'})
-    res.end("welcome to my portfolio")
-})
-
-Server.listen(4000, '127.0.0.1')
-console.log("I have created a server for my portfolio!")
--- file system.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs');
-fs.writeFile('myFirstFile.txt','Hello World',(err) =>{
-    if (err) throw err;
-    console.log('file created successfully')
-})
